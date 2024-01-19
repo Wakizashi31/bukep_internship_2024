10748b0b04fcaab22aeb5ca66c03e8330f172e5d
Wakizashi31 <119972118+Wakizashi31@users.noreply.github.com>
2024-01-19 11:57:55 +0300

    Lab5
    * FirstTry
    
    * Завершил 5 лабораторную работу

--- student_323431/BUKEP.Student.ConsoleCalculator/BUKEP.Student.ConsoleCalculator/BUKEP.Student.ConsoleCalculator/Program.cs
+++ student_323431/BUKEP.Student.ConsoleCalculator/BUKEP.Student.ConsoleCalculator/BUKEP.Student.ConsoleCalculator/Program.cs
@@ -1,53 +1,37 @@
-﻿namespace BUKEP.Student.ConsoleCalculator
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+
+namespace Calculator
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-
             while (true)
             {
-                Console.WriteLine("\r\nВведите простое математическое выражение для вычисления операции (+ или - или * или /) над двумя числами.\nПо завершению ввода операции нажмите Enter:");
-                string mathExpression = Console.ReadLine();
-
-                char[] symbolsMath = new char[] { '+', '-', '/', '*' };
-                string[] numbers = mathExpression.Split(symbolsMath);
-                if (numbers.Length >= 3)
-                {
-                    Console.WriteLine("В выражении должно быть только 2 числа");
-                    continue;
-                }
+                Console.WriteLine("\r\nВведите простое математическое выражение для вычисления операции (+ или - или * или /)\nПо завершению ввода операции нажмите Enter:");
+                string input = Console.ReadLine();
                 try
                 {
-                    int numberOne = int.Parse(numbers[0]);
-                    int numberTwo = int.Parse(numbers[1]);
-                    char s = symbolsMath.First(s => mathExpression.Any(os => os == s));
-                    {
-                        switch (s)
-                        {
-                            case '+':
-                                Console.WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}");
-                                break;
-                            case '-':
-                                Console.WriteLine($"{numberOne} - {numberTwo} = {numberOne - numberTwo}");
-                                break;
-                            case '/':
-                                if (numberTwo == 0)
-                                    Console.WriteLine("На 0 делить нельзя");
-                                else
-                                    Console.WriteLine($"{numberOne} / {numberTwo} = {(float)(numberOne / (float)numberTwo)}");
-                                break;
-                            case '*':
-                                Console.WriteLine($"{numberOne} * {numberTwo} = {numberOne * numberTwo}");
-                                break;
-
-                        }
-                    }
+                    string RPN = ConvertToRPN(input);
+                    double result = CalculateRPN(RPN);
+                    Console.WriteLine("Result: " + result);
 
                 }
-                catch
+                catch (DivideByZeroException)
                 {
-                    Console.WriteLine("Неверный ввод!");
+                    Console.WriteLine("Деление на ноль");
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("Неизвестная операция");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"ошибка: {ex}");
                 }
                 while (true)
                 {
@@ -63,10 +47,110 @@
                     {
                         break;
                     }
+                    else
+                    {
+                        Console.WriteLine(" - Введена неизвестная команда. Повторите ввод.");
+                        continue;
+                    }
                 }
-            
+            }
+        }
+
+        /// <summary>
+        /// Метод возвращающий результат выражения из обратной польской записи
+        /// </summary>
+        /// <param name="rpn">Математическое выражение</param>
+        /// <returns>Результат математичесокго выражения</returns>
+        /// <exception cref="InvalidOperationException">Неизвестная операция</exception>
+        /// <exception cref="DivideByZeroException">Деление на ноль</exception>
+        static double CalculateRPN(string rpn)
+        {
+            var stack = new Stack<double>();
+
+            var elements = rpn.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var element in elements)
+            {
+                if (double.TryParse(element, NumberStyles.Any, CultureInfo.InvariantCulture, out double number))
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    double right = stack.Pop();
+                    double left = stack.Pop();
+
+                    switch (element)
+                    {
+                        case "+": stack.Push(left + right); break;
+                        case "-": stack.Push(left - right); break;
+                        case "*": stack.Push(left * right); break;
+                        case "/":
+                            if (right == 0)
+                                throw new DivideByZeroException();
+                            stack.Push (left / right);
+                            break;
+                        default: throw new InvalidOperationException("Неизвестная операция");
+                    }
+                }
             }
 
+            return stack.Pop();
+        }
+
+        /// <summary>
+        /// Метод преобразует математическое выражение в формат обратной польской записи 
+        /// </summary>
+        /// <param name="input">Математическое выражение</param>
+        /// <returns>Математическое выражение в формате обратной польской записи</returns>
+        static string ConvertToRPN(string input)
+        {
+            var operators = new Dictionary<char, int>
+            {
+                ['('] = 0,
+                [')'] = 1,
+                ['+'] = 2,
+                ['-'] = 2,
+                ['*'] = 3,
+                ['/'] = 3
+            };
+
+            var output = new StringBuilder();
+            var stack = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                if (Char.IsDigit(c) || c == '.')
+                    output.Append(c);
+
+                else if (operators.ContainsKey(c))
+                {
+                    output.Append(' ');
+
+                    if (stack.Count == 0 || c == '(')
+                        stack.Push(c);
+
+                    else if (c == ')')
+                    {
+                        while (stack.Peek() != '(')
+                        {
+                            output.Append(stack.Pop() + " ");
+                        }
+                        stack.Pop();
+                    }
+
+                    else
+                    {
+                        while (stack.Count != 0 && operators[c] <= operators[stack.Peek()])
+                            output.Append(stack.Pop() + " ");
+                        stack.Push(c);
+                    }
+                }
+            }
+
+            while (stack.Count != 0)
+                output.Append(" " + stack.Pop());
+
+            return output.ToString();
         }
     }
 }
