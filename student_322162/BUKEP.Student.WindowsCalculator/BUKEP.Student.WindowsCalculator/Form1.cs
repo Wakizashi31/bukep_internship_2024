5e051c85e5f0f9f13426dd085c1260d05817521c
AzarDimka <149622306+AzarDimka@users.noreply.github.com>
2024-01-31 11:10:33 +0300

    Завершение 6 лабораторной (#6)
    * Завершение 6 лабораторной

---
+++ student_322162/BUKEP.Student.WindowsCalculator/BUKEP.Student.WindowsCalculator/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BUKEP.Student.WindowsCalculator
+{
+    public partial class Form1 : Form
+    {
+        private InputHandler InputHandler = new InputHandler();
+        private Calculator Calculator = new Calculator();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonInput_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            char newChar = button.Text[0]; 
+            textBox1.Text = InputHandler.GetUpdatedInput(textBox1.Text, newChar);
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void ButtonDeleteChar_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+            }
+        }
+
+        private void ButtonResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool symbolEqual = textBox1.Text.Contains('=');
+
+                if (!symbolEqual)
+                {
+                    textBox1.Text += " = " + Calculator.Calculate(textBox1.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Результат выражения уже получен.");
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Деление на ноль.");
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Математическое выражение не удалось преобразовать.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex);
+            }
+        }
+    }
+}
