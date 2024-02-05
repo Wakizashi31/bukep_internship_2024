8322f9d3a73e98ff0fed9672faf91da6a1e6869d
Dima <azardimka@gmail.com>
2024-02-05 13:39:38 +0300

    Выполнил рабораторную 7

---
+++ student_322162/BUKEP.Student/BUKEP.Student.ConsoleCalculator/BUKEP.Student.ConsoleCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Globalization;
+using BUKEP.Student.Calculator;
+
+namespace BUKEP.Student.ConsoleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleCalculator();
+        }
+        static void ConsoleCalculator()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите математическое выражение.\nПо завершению ввода операции нажмите Enter:");
+                string input = Console.ReadLine();
+
+                try
+                {
+                    Calculator.Calculator calculator = new Calculator.Calculator();
+
+                    double result = calculator.Calculate(input);
+                    Console.WriteLine($"Результат выражения: {result}");
+                }
+                catch(DivideByZeroException)
+                {
+                    Console.WriteLine($"Ошибка: Деление на ноль.");
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Ошибка: Недопустимое выражение");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex}");
+                }
+                
+                while (true)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Для повторного ввода операции нажмите Enter, для завершения приложения Esc.");
+                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                    if (keyInfo.Key == ConsoleKey.Escape)
+                    {
+                        return;
+                    }
+                    else if (keyInfo.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(" - Введена неизвестная команда. Повторите ввод.");
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
