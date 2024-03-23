07e7cb7c38ca9f75284af6d41f01f1620a9f283f
Wakizashi31 <Nevermore190102@gmail.com>
2024-03-23 20:58:48 +0300

    Добавил ввод кнопок 11 работа

---
+++ student_323431/BUKEP.Student/BUKEP.Student.MvcCalculator/BUKEP.Student.MvcCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BUKEP.Student.MvcCalculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }   
+}
