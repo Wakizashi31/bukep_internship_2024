cb5b4ec8b947725fa2a4963f1094e8ed32744ebb
Wakizashi31 <Nevermore190102@gmail.com>
2024-03-26 17:27:02 +0300

    Бд не работает

--- student_323431/BUKEP.Student/BUKEP.Student.MvcCalculator/BUKEP.Student.MvcCalculator/Controllers/CalculatorController.cs
+++ student_323431/BUKEP.Student/BUKEP.Student.MvcCalculator/BUKEP.Student.MvcCalculator/Controllers/CalculatorController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BUKEP.Student.Calculator;
+using BUKEP.Student.Calculator.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
@@ -10,11 +12,41 @@ namespace BUKEP.Student.MvcCalculator.Controllers
 {
     public class CalculatorController : Controller
     {
-        
+        private readonly ICalculationResultService _resultService;
+        private readonly MathCalculator _mathCalculator;
+        public CalculatorController(ICalculationResultService resultService, MathCalculator mathCalculator)
+        {
+            _resultService = resultService;
+            _mathCalculator = mathCalculator;
+        }
+
         public ActionResult Index()
         {
             return View();
         }
-        
+
+        [HttpPost]
+        public JsonResult CalculateResult(string expression)
+        {
+            try
+            {
+                double result = _mathCalculator.ResultCalculate(expression);
+                return Json(result);
+            }
+            catch (DivideByZeroException) 
+            {
+                return Json("Деление на ноль!");
+            }
+            catch(InvalidOperationException)
+            {
+                return Json("Неверное выражение");
+            }
+        }
+
+        [HttpPost]
+        public void SaveResult(string expression)
+        {
+            _resultService.Save(Convert.ToDouble(expression));
+        }
     }   
 }
